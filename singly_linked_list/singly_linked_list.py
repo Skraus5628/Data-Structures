dfa8b824022474c7fde1222c54f9866c195e0446
Samuel Kraus <53798819+Skraus5628@users.noreply.github.com>
2020-08-19 20:54:45 -0500

    day 1 mvp

--- singly_linked_list/singly_linked_list.py
+++ singly_linked_list/singly_linked_list.py
@@ -1,61 +1,84 @@
-
-
-class Node: 
+class Node:
     def __init__(self, value, next=None):
         self.value = value
-        self.next = next #Nxt node in the list
-
-
+        self.next = next  # The next node in the list
 class LinkedList:
     def __init__(self):
-        self.head: Node = None #Points to the first node in the list
-        self.tail: Node = None  #Points to the last node in the list
+        self.head: Node = None  # points to the first node in the list
+        self.tail: Node = None  # Points to the last node in the list
         self.length = 0
-    # append / add ==> add_to_tail
-    
+    def __str__(self):
+        pass
     def add_to_tail(self, value):
-        #Check if there's a tail
-        #If there is no tail (empty list)
-        if not self.tail: #could also check length, check if tail = None
-        # Create a new node
+        # Check if there's a tail
+        # If there is no tail (empty list)
+        if self.tail is None:
+            # Create a new node
             new_tail = Node(value, None)
-        # set self, head and self.tail 
-
-        #set current tail.next to the new node
+        #    Set self.head and self.tail to the new node
+            self.head = new_tail
+            self.tail = new_tail
+        # If there is a tail (general case):
+        else:
+            # Create a new node with the value we want to add, next = None
+            new_tail = Node(value, None)
+            # Set current tail.next to the new node
             old_tail = self.tail
             old_tail.next = new_tail
-
-
-
-
-    #remove
-
+            # Set self.tail to the new node
+            self.tail = new_tail
+        self.length += 1
     def remove_head(self):
-        #IF not head (empty list)
-        if not self.head:
+        # If not head (empty list)
+        if self.head is None:  # if self.head is None
             return None
-
-        #List with one element:
+        # List with one element:
         if self.head == self.tail:
-        # Set self head to current_head.next(Which is also None)
+            # Set self.head to current_head.next / None
             current_head = self.head
             self.head = None
-        #Set self.tail to none
+            # Set self.tail to None
             self.tail = None
-            self.length -= 1 #( -= is same as self.length -1)
+            # Decrement length by 1
+            self.length = self.length - 1
             return current_head.value
-        #Decrement lengthy by 1
-
-        #If head(general case):
-        else: 
-        #Set self, head to current_head.next
+        # If head (General case):
+        else:
+            # 	Set self.head to current_head.next
             current_head = self.head
             self.head = current_head.next
-                #return current_head.value
-            self.length = self.length -1
+            #  Return current_head.value
+            self.length -=1
             return current_head.value
-
-
-
     def remove_tail(self):
-        pass
+        if self.tail is None:
+            return None
+        if self.tail == self.head:
+           current_tail = self.tail
+           self.tail = None
+           self.head = None
+           self.length -=1
+           return current_tail.value
+        else:
+            current_node = self.head
+            while current_node.next != self.tail:
+                current_node = current_node.next
+            current_tail = self.tail
+            self.tail = current_node
+            current_node.next = None
+            self.length -=1
+            return current_tail.value
+
+        # Remove Tail:
+        # Check if it's there
+        # General case:
+        # Start at head and iterate to the next-to-last node
+        # Stop when current_node.next == self.tail
+        # Save the current_tail value
+        # Set self.tail to current_node
+        # Set current_node.next to None
+        #
+        # List of 1 element:
+        # Save the current_tail.value
+        # Set self.tail to None
+        # Set self.head to None
